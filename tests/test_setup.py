70e5a94d39e604d7a6279d335b677655f2e9dc21
EF <jairusheredia@gmail.com>
2024-07-29 10:31:59 +0800

    updated tests try 4

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -5,6 +5,3 @@ pytest.skip(reason="This test is a cli based test", allow_module_level=True)
 
 def test_main():
     subprocess.call('python -m pygstudio create "Test Pygstudio Project" -o ".temp"')
-    
-def test_dummy():
-    pass
