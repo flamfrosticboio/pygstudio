5bc0f8c2e7bf1c41412fd24f8cd7ddf5ef24100a
EF <jairusheredia@gmail.com>
2024-07-29 10:24:36 +0800

    updated tests

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -2,6 +2,6 @@ import subprocess
 from datetime import datetime
 import pytest
 
-@pytest.skip(reason="This test is a cli based test")
+@pytest.skip(reason="This test is a cli based test", allow_module_level=True)
 def main():
     subprocess.call('python -m pygstudio create "Test Pygstudio Project" -o ".temp"')
