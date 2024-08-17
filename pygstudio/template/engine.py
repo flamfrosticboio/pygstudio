13fc8b1e561fd530cc6d8c83f8e6a02b1f540f2f
flamfrosticboio <105685349+flamfrosticboio@users.noreply.github.com>
2024-08-17 13:11:03 +0800

    updated template files

---
+++ pygstudio/template/engine.py
@@ -0,0 +1,27 @@
+# // Pygstudio template file created by pygstudio script (Version 1.3.1) 
+# ? You are free to edit this script
+
+def start(screen_size, screen_fill_color, window_flags, window_title, window_icon, fps, vsync):
+    import pygame, globals, script_manager
+    
+    globals.screen = pygame.display.set_mode(screen_size, window_flags, 0, 0, vsync)
+    pygame.display.set_caption(window_title)
+    if window_icon: pygame.display.set_icon(window_icon)
+    
+    globals.on_init()
+    script_manager.run()
+    
+    globals.running = True
+    while globals.running == True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                globals.running = False
+            globals.on_event(event)
+        
+        globals.screen.fill(screen_fill_color)
+        globals.on_render(globals.screen)
+        
+        pygame.display.flip()
+        globals.clock.tick(fps)
+        
+    globals.on_exit()
