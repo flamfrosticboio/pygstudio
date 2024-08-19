c870d269317a6463d94d8667782ca97f480bae04
flamfrosticboio <105685349+flamfrosticboio@users.noreply.github.com>
2024-08-19 16:40:51 +0800

    completed config functionalities at least

--- pygstudio/config.py
+++ pygstudio/config.py
@@ -2,53 +2,58 @@ from os.path import join, exists, abspath
 from os import remove
 from json import loads, dumps
 
-from .shared import print_error, print_log
-
-# Do not change default value. Trust me
-DEFAULT_CONFIGURATION = {
-    "AdditionalCreatePath": None,
-}
+from .shared import *
 
 config_path = abspath(join(__file__, "..", "config.json"))
-REMOVE_KEY = "REMOVE"
+
+# Do not change default value to often. Trust me
+DEFAULT_CONFIGURATION = {
+    "additional_create_path": None,
+    "default_pyinstaller_args": "-w --noconfirm",
+}
 
 
-def remove_duplicate(dict_a: dict, dict_b: dict):
-    for key in dict_a.copy():
-        value_a = dict_a.get(key)
-        value_b = dict_b.get(key)
-        if value_a != value_b and value_a != REMOVE_KEY:
-            continue
-        dict_a.pop(key)
+def print_table(table):
+    for key in table:
+        print(f"| \033[93m{key}\033[0m = \033[34m{repr(table.get(key))}\033[0m")
 
-
-def change_config(config):
-    new_config_table = get_config()
-
-    if config.additional_create_path:
-        if config.additional_create_path == REMOVE_KEY:
-            new_config_table["AdditionalCreatePath"] = REMOVE_KEY  # type: ignore the stupid dict[str, None]
-        else:
-            path = abspath(config.additional_create_path)
-            if not exists(path):
-                print_error(f"Invalid file/folder: '{path}'")
-                return
-            new_config_table["AdditionalCreatePath"] = path
-            
-    remove_duplicate(new_config_table, DEFAULT_CONFIGURATION)
-    
-    if len(new_config_table) <= 0:
-        if exists(config_path):
-            remove(config_path)
-        return 
-
-    with open(config_path, "wt") as file:
-        print_log("Your new pygstudio configuration:")
-        print(new_config_table)
-        file.write(dumps(new_config_table))
 
 def print_failed_config_details():
     print_log(f"Details: config_file={config_path}")
+
+
+def save_configuration(new_config_table):
+    if len(new_config_table) <= 0:
+        if exists(config_path):
+            remove(config_path)
+        return
+
+    with open(config_path, "wt") as file:
+        print_log("Your new pygstudio configuration:")
+        print_table(new_config_table)
+        file.write(dumps(new_config_table))
+
+
+def change_configuration(config):
+    new_config_table = get_config()
+
+    for key in DEFAULT_CONFIGURATION:
+        if not getattr(config, key):
+            continue
+        new_config_table[key] = getattr(config, key)
+
+    save_configuration(new_config_table)
+
+
+def get_configuration(config):
+    table = get_config()
+
+    print_success(f"config_path={config_path}")
+    print_log("Your configuration table:")
+
+    keys = {key for key in DEFAULT_CONFIGURATION if getattr(config, key, False) == True}
+    print_table(table if len(keys) == 0 else keys)
+
 
 def get_config():
     new_config = DEFAULT_CONFIGURATION.copy()
@@ -65,3 +70,26 @@ def get_config():
         print_failed_config_details()
 
     return new_config
+
+def reset_config(config):
+    table = get_config()
+
+    print_success(f"config_path={config_path}")
+    
+    for key in DEFAULT_CONFIGURATION:
+        if not getattr(config, key):
+            continue
+        table[key] = DEFAULT_CONFIGURATION[key]
+    
+    save_configuration(table)
+
+def main(config):
+    print(config)
+    if config.config_command == "set":
+        change_configuration(config)
+    elif config.config_command == "get":
+        get_configuration(config)
+    elif config.config_command == "remove":
+        reset_config(config)
+    else:
+        print_error("No arguments provided. Type `pygstudio config -h` for help.")
