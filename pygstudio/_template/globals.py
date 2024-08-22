88db23c27a7c25ad04466b1584b30f5a82d91398
flamfrosticboio <105685349+flamfrosticboio@users.noreply.github.com>
2024-08-22 15:39:39 +0800

    updated on_init to take a screen arg

--- pygstudio/_template/globals.py
+++ pygstudio/_template/globals.py
@@ -1,4 +1,4 @@
-# // Pygstudio template file created by pygstudio script (Version 1.2)
+# // Pygstudio template file created by pygstudio script (Version 1.2.1)
 # ? You are free to edit this script
 
 import pygame
@@ -7,7 +7,7 @@ import pygame
 running: bool = False
 clock = pygame.time.Clock()
 # The screen is None if the window is not initialized
-screen = None                       # type: pygame.Surface | None
+screen = None   # type: pygame.Surface | None
 
 def on_event(event: pygame.event.Event) -> None:
     # * This bindable function is called in every event.
@@ -17,7 +17,7 @@ def on_render(screen: pygame.Surface) -> None:
     # * This bindable function is executed in every rendered step.
     pass
 
-def on_init():
+def on_init(screen: pygame.Surface):
     # * This bindable function is executed after the window initialization
     pass
 
