5e0991155e3ee114582e0d2061b298e4192031ab
flamfrosticboio <105685349+flamfrosticboio@users.noreply.github.com>
2024-08-26 15:05:19 +0800

    prettify

--- scripts/pack_template.py
+++ scripts/pack_template.py
@@ -6,13 +6,18 @@ template_folder = os.path.join(pygstudio_path, "_template")
 if not os.path.exists(template_folder):
     raise RuntimeError("Cannot pack template if template folder is none.")
 
-remove_paths = [os.path.join(pygstudio_path, "template.zip"), os.path.join(pygstudio_path, "template"),
-                os.path.join(pygstudio_path, "_template", "__pycache__")]
+remove_paths = [
+    os.path.join(pygstudio_path, "template.zip"),
+    os.path.join(pygstudio_path, "template"),
+    os.path.join(pygstudio_path, "_template", "__pycache__"),
+]
 for path in remove_paths:
     if os.path.exists(path):
         print(f"Removing {path}")
-        if os.path.isfile(path): os.remove(path)
-        else: shutil.rmtree(path)
+        if os.path.isfile(path):
+            os.remove(path)
+        else:
+            shutil.rmtree(path)
 
 shutil.make_archive(os.path.join(pygstudio_path, "template"), "zip", template_folder)
 print("Operation was successful")
